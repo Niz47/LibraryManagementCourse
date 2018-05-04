b8ef1f310d6f4469fd30abb436aeed8a6929775d
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-05-04 17:51:57 +0630

    finish Part-14 >> add Model,Repository,ViewModel,CustomerController,Service

---
+++ LibraryManagementCourse/Data/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementCourse.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementCourse.Data.Interfaces
+{
+    public interface IAuthorRepository : IRepository<Author>
+    {
+        IEnumerable<Author> GetAllWithBooks();
+
+        Author GetWithBooks(int id);
+    }
+}
